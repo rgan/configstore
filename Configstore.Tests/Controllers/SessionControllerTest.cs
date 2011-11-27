557ae4bf87c1f7dbf4767119f42a842de1974db9
rgan <rajaram.ganeshan@gmail.com>
2011-11-27 11:29:34 -0800

    Added session controller

---
+++ Configstore.Tests/Controllers/SessionControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using MvcContrib.TestHelper;
+using NUnit.Framework;
+using Configstore.Controllers;
+
+namespace Configstore.Tests.Controllers
+{
+    [TestFixture]
+    public class SessionControllerTest
+    {
+        [Test]
+        public void ShouldSetHitsOnSession()
+        {
+            var builder = new TestControllerBuilder();
+            var controller = new SessionController();
+            builder.InitializeController(controller);
+
+            controller.Index();
+            Assert.IsNotNull(controller.Session["hits"]);
+        }
+    }
+}
---
+++ Configstore/Controllers/SessionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Configstore.Controllers
+{
+    public class SessionController : Controller
+    {
+        //
+        // GET: /Session/
+
+        public ActionResult Index()
+        {
+            int hits = 0;
+            if (Session["hits"] != null)
+            {
+                hits = (int) Session["hits"];
+            }
+            Session["hits"] = hits + 1;
+            return View();
+        }
+
+    }
+}
